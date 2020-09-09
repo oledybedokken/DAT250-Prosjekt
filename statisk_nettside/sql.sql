29c9d1bee589da570449a25129ad83aaa7f24ac0
Pervaz Golrang <pe.golrang@stud.uis.no>
2020-09-09 11:31:54 +0200

    Basic sql

---
+++ statisk_nettside/sql.sql
@@ -0,0 +1,39 @@
+-- MySQL
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+CREATE TABLE IF NOT EXISTS 'brukere' (
+    'id' int(5) NOT NULL AUTO_INCREMENT,
+    'email' varchar(32)) NOT NULL,
+    'etternavn' varchar(32) NOT NULL,
+    'fornavn' varchar(32) NOT NULL,
+    'bruker_nr' varchar(16) NOT NULL,
+    'dagly_transaksjon_limit' varchar(8) NOT NULL,      -- daglig transaksjons limit. 8 siffer
+    'mengde_overfor_hoy' varchar(6) NOT NULL,           -- TRENGER AUTHENTICATION KONFIRMATION, MAKS 10'000'000
+    'mengde_overfor_lav' varchar(5) NOT NULL,           -- antall sendt over (log), MAKS 10'0000
+    'antall_fra_deg' varchar(10) NOT NULL,              -- Antall briler har gitt
+    'antall_fra_andre' varchar(10) NOT NULL,            -- Antall bruker har fått
+    'brukskonto' varchar(7) NOT NULL,                   -- MAKS 10'000'000
+    UNIQUE KEY 'bruker_nr' ('bruker_nr'),
+    UNIQUE KEY 'email' ('email'),
+    PRIMARY KEY ('id')
+)
+
+CREATE TABLE IF NOT EXISTS `kunder` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `fornavn` varchar(32) NOT NULL,
+  `etternavn` varchar(32) NOT NULL,
+  `fodselsdato` varchar(6) NOT NULL,               -- 310100 som betyr DD/MM/ÅÅ
+  `tlf_nummer` varchar(8) NOT NULL,                -- 82 74 82 42 59
+  `email` varchar(32) NOT NULL,
+  `passord` varchar(32) NOT NULL,
+  `authentication` varchar(5) NOT NULL,            -- Authentication kode, random
+  `konto_nr` varchar(16) NOT NULL,
+  `by` varchar(16) NOT NULL,
+  'fylke' varchar(16) NOT NULL,
+  `post` varchar(3) NOT NULL,
+  `post_kode` varchar(5) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `account_number` (`account_number`),
+  UNIQUE KEY `email` (`email`)
+)
