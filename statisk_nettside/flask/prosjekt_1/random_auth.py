0123d4c55d2e032a3924943ea7317d05415fbf6e
Pervaz Golrang <pe.golrang@stud.uis.no>
2020-09-16 22:50:56 +0200

    skrivefeil

--- statisk_nettside/flask/prosjekt_1/random_auth.py
+++ statisk_nettside/flask/prosjekt_1/random_auth.py
@@ -5,7 +5,7 @@ app : Flask = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def myRandom():
-    r1 = random.uniform(10000, 9999)
+    r1 = random.uniform(10000, 99999)
     return r1
 
 if __name__ == "__main__":
