12d4933d8812ccfdce61ae1ff2723df2eddfa60f
Pervaz Golrang <pe.golrang@stud.uis.no>
2020-09-16 22:48:10 +0200

    rename modul fil

--- statisk_nettside/flask/prosjekt_2/alle_moduler.py
+++ statisk_nettside/flask/prosjekt_2/alle_moduler.py
@@ -2,17 +2,7 @@ from flask import (render_template, Blueprint, request, make_response, send_from
 import jwt
 import decimal as decimal
 from datetime import datetime
-# from .api.utils import to_d128, withdraw, deposit
-# from bson.decimal128 import create_decimal128_context, Decimal128
-# from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity, get_raw_jwt
-# from .config import Config
-# from .model import clients
-# from .model import get_account_num
-# from .model import jti_backlist
-# from luhn import verify
-# from . import mongo 
-# from . import bcrypt
-# from . import f_jwt
+
 
 # Copiert forklaring fra flask.palletsprojects.com:
 
--- statisk_nettside/flask/prosjekt_2/config.py
+++ statisk_nettside/flask/prosjekt_2/config.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import os
 
-class DevelopmentConfig(Config):
+class DevConfig(Config):
     DEBUG = True
     SECRET_KEY = "dev"
 
-class ProductionConfig(Config):
-    __secret = "admin" #"1lw3X$Jy*T5^hue"
-    SECRET_KEY = os.environ.get("SECRET_KEY", __secret)
+class ProdConfig(Config):
+    __nokkel = "admin" #"1lw3X$Jy*T5^hue"
+    SECRET_KEY = os.environ.get("SECRET_KEY", __nokkel)
