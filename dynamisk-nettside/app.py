77e45743a7059b877b81fb567db8e44ef36e4ca6
JStorsveen <54702177+JStorsveen@users.noreply.github.com>
2020-09-26 15:40:18 +0200

    Dynamisk oversikt

--- dynamisk-nettside/app.py
+++ dynamisk-nettside/app.py
@@ -17,8 +17,27 @@ class User:
         self.saldo = int(saldo)
         self.dato = dato
 
+        # Liste med kontoer og lån
+        self.kontoer = []
+        self.laan = []
+
     def __repr__(self):
         return f'<User: {self.username}>'
+
+
+class BankAccount:
+    def __init__(self, name, kontotype, user_id, saldo):
+        self.name = name
+        self.kontotype = kontotype
+        self.user_id = user_id
+        self.saldo = int(saldo)
+
+
+class Loan:
+    def __init__(self, name, user_id, saldo):
+        self.name = name
+        self.user_id = user_id
+        self.saldo = int(saldo)
 
 users = []
 users.append(User(id = 1, username= "Ole", password ="password", saldo = 2, dato =dt.date(2002, 5, 6) ))
@@ -26,6 +45,12 @@ users.append(User(id = 2, username= "Pervaz", password ="password2", saldo=500,d
 users.append(User(id = 3, username= "Espen", password ="69420123", saldo=1000, dato =dt.date(2002, 5, 6)))
 users.append(User(id = 4, username= "Jørgen", password ="password3", saldo=600 , dato =dt.date(2002, 5, 6)))
 
+for user in users:
+    user.kontoer.append(BankAccount(name = "Brukskonto", kontotype ="bruk", user_id = 4, saldo = 1300))
+    user.kontoer.append(BankAccount(name = "Regninger", kontotype = "bruk", user_id = 4, saldo = 7200))
+    user.kontoer.append(BankAccount(name = "Sparekonto", kontotype = "spar", user_id = 4, saldo = 68925))
+    user.laan.append(Loan(name = "Lån type hus", user_id = 4, saldo = 2345928))
+    user.laan.append(Loan(name = "Lån type bil", user_id = 4, saldo = 356281))
 
 app = Flask(__name__)
 app.secret_key = 'brusjanbank'
