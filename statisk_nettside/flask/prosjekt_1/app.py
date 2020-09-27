25be8f6a5ef556972f6df400b523cdc394bc648a
Pervaz Golrang <pe.golrang@stud.uis.no>
2020-09-27 17:26:47 +0200

    forandret server adresse

--- statisk_nettside/flask/prosjekt_1/app.py
+++ statisk_nettside/flask/prosjekt_1/app.py
@@ -534,4 +534,4 @@ def kontolog():
 if __name__ == "__main__":
     app.secret_key = "admin123"
     app.debug = True
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="127.0.0.1", port=5000)
