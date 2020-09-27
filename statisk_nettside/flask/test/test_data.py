7093cb1caecf178d9bbab089ffc821c224654287
Pervaz Golrang <pe.golrang@stud.uis.no>
2020-09-27 19:11:14 +0200

    test_data.py for database-testing

--- statisk_nettside/flask/test/test_data.py
+++ statisk_nettside/flask/test/test_data.py
@@ -25,7 +25,7 @@ def kontoer():
     email = "sjamokn@gmail.com"
     navn = "Watermalaon"
     usert = "cashier"
-    passord = "Suresh@002"
+    passord = "Fritok123"
     passord_hash = bcrypt.generate_password_hash(passord).decode("utf-8")
     db.execute("INSERT INTO bruker (id,navn,bruker_type,passord) VALUES (:u,:n,:t,:p)", {"u": email,"n":navn,"t":usert ,"p": passord_hash})
     db.commit()
@@ -33,9 +33,11 @@ def kontoer():
     email = "koimnbn@gmail.com"
     navn = "Lakim"
     usert = "teller"
-    passord = "Mahesh@003"
+    passord = "Lakim123"
     passord_hash = bcrypt.generate_password_hash(passord).decode("utf-8")
     db.execute("INSERT INTO bruker (id,navn,bruker_type,passord) VALUES (:u,:n,:t,:p)", {"u": email,"n":navn,"t":usert ,"p": passord_hash})
     db.commit()
     print("Konto skapes ............................................ ")
 
+if __name__ == "__main__":
+    kontoer()
