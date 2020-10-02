2d6b10ab179c9b3ff82387dfeabc1acc2b18a5ef
Pervaz Golrang <pe.golrang@stud.uis.no>
2020-10-02 17:46:23 +0200

    flyttet forms til models.py

--- project/app.py
+++ project/app.py
@@ -2,6 +2,7 @@ from flask import Flask,g, redirect, render_template, request,session, url_for, 
 import datetime as dt
 from datetime import datetime, timedelta
 import random
+from models import User, Transaksjoner, Loan, Bankaccount
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin,current_user, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -21,41 +22,6 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50),nullable=False)
-    fornavn = db.Column(db.String(50),nullable=False)
-    etternavn = db.Column(db.String(50),nullable=False)
-    postAddresse = db.Column(db.String(50),nullable=False)
-    postKode = db.Column(db.String(50),nullable=False)
-    fylke = db.Column(db.String(50),nullable=False)
-    kjonn = db.Column(db.String(50),nullable=False)
-    fodselsdato = db.Column(db.String(50),nullable=False)
-
-
-class Transaksjoner(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    tidspunkt = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
-    verdi = db.Column(db.Integer)
-    KID = db.Column(db.String(15))#KIDnr skal være 15 lange ifølge ole
-    konto = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-
-
-class Loan(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    verdi = db.Column(db.Integer,nullable = False)
-    rente = db.Column(db.Integer, nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-
-class BankAccount(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    navn = db.Column(db.String(50),nullable=False)
-    kontotype = db.Column(db.String(50),nullable=False)
-    saldo = db.Column(db.Integer,nullable = False)
-    user_id = db.Column(db.Integer, nullable = False)
 
 @login_manager.user_loader
 def load_user(user_id):
--- project/models.py
+++ project/models.py
@@ -3,8 +3,42 @@
 from flask_login import UserMixin
 from . import db
 
+#class User(UserMixin, db.Model):
+#    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+#    email = db.Column(db.String(100), unique=True)
+#    password = db.Column(db.String(100))
+#    name = db.Column(db.String(1000))
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50),nullable=False)
+    fornavn = db.Column(db.String(50),nullable=False)
+    etternavn = db.Column(db.String(50),nullable=False)
+    postAddresse = db.Column(db.String(50),nullable=False)
+    postKode = db.Column(db.String(50),nullable=False)
+    fylke = db.Column(db.String(50),nullable=False)
+    kjonn = db.Column(db.String(50),nullable=False)
+    fodselsdato = db.Column(db.String(50),nullable=False)
+
+
+class Transaksjoner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tidspunkt = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+    verdi = db.Column(db.Integer)
+    KID = db.Column(db.String(15))#KIDnr skal være 15 lange ifølge ole
+    konto = db.Column(db.Integer)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+class Loan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    verdi = db.Column(db.Integer,nullable = False)
+    rente = db.Column(db.Integer, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+class BankAccount(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    navn = db.Column(db.String(50),nullable=False)
+    kontotype = db.Column(db.String(50),nullable=False)
+    saldo = db.Column(db.Integer,nullable = False)
+    user_id = db.Column(db.Integer, nullable = False)
