3acdf2a5f5c98eea8b32e82994310ff54cadca81
JStorsveen <54702177+JStorsveen@users.noreply.github.com>
2020-10-02 23:13:40 +0200

    .

--- project/models.py
+++ project/models.py
@@ -1,7 +1,7 @@
 from flask_login import UserMixin
 from datetime import datetime, timedelta
 from flask_sqlalchemy import SQLAlchemy
-from . import db
+from .__init__ import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
