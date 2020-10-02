8a0305772d81f5398cd807b7bbecfde81093a5e2
Pervaz Golrang <pe.golrang@stud.uis.no>
2020-10-02 17:55:58 +0200

    oppdatert traksaksjoner

--- project/models.py
+++ project/models.py
@@ -4,6 +4,12 @@ from flask_login import UserMixin
 import datetime as dt
 from datetime import datetime, timedelta
 
+#class User(UserMixin, db.Model):
+#    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+#    email = db.Column(db.String(100), unique=True)
+#    password = db.Column(db.String(100))
+#    name = db.Column(db.String(1000))
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True, nullable=False)
@@ -23,11 +29,14 @@ class Deposit(db.Model):
     amount = db.Column(db.Integer(50), nullable = False)
 
 class Transaksjoner(db.Model):
+    __tablename__ = 'transaksjoner'
     id = db.Column(db.Integer, primary_key=True)
     tidspunkt = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+    beskrivelse = db.Column(db.Text)
     verdi = db.Column(db.Integer)
     KID = db.Column(db.String(15))#KIDnr skal være 15 lange ifølge ole
     konto = db.Column(db.Integer)
+    date = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
 
 class Loan(db.Model):
