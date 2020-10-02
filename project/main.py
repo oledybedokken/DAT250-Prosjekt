e728c66ba6b29258959e7629159d680853f9a7bf
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2020-10-02 18:55:59 +0200

    bank

--- project/main.py
+++ project/main.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Flask,g, redirect, render_template, request,session, url_for, flash, Blueprint
 from flask_login import login_required, current_user
+from .models import User, Transaksjoner, Loan, BankAccount
+import random
 
 main = Blueprint('main', __name__)
 
@@ -10,4 +12,23 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.navn)
+    return render_template('profile.html', name=current_user.fornavn)
+
+@main.route('/overview')
+@login_required
+def overview():
+    kontoer=BankAccount.query.filter_by(user_id=current_user.id).all()
+    return render_template('overview.html', kontoer=kontoer)
+
+@main.route('/create_bank_account', methods=['GET', 'POST'])
+@login_required
+def create_bank_account():
+    if request.method == 'POST':
+        kontotype = request.form['kontotype']
+        kontonavn = request.form['kontonavn']
+        new_account = BankAccount(id = int(random.randint(1e15, 1e16)), navn = kontonavn, kontotype = kontotype, saldo=int(0), user_id = current_user.id)
+        db.session.add(new_account)
+        db.session.commit()
+        return redirect(url_for('overview'))
+
+    return render_template('create_bank_account.html')
